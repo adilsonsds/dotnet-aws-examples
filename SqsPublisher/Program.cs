ee198801496734bd31c9a3deabd050fef08a6862
Adilson Silva <adilsonsds@icloud.com>
2024-02-14 12:47:13 -0300

    Creating project SqsPublisher

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "adilsonsds@teste.com",
+    FullName = "Adilson Silva",
+    DateOfBirth = new DateTime(1993, 12, 30),
+    GitHubUserName = "adilsonsds"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    },
+    // DelaySeconds = 30
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
